76999acf65267b9783fdce12994a320248130308
CerminatiPaula <85971540+CerminatiPaula@users.noreply.github.com>
2021-10-15 13:05:58 -0300

    Add files via upload

---
+++ src/components/nav.js
@@ -0,0 +1,15 @@
+const Nav = () => {
+	return (
+		<>
+		<nav > 
+			<div id="nav">
+				<a href="#"><h2> Tienda</h2></a>
+				<a href="#"><h2>Nuestras cervezas</h2></a>
+				<a href="#"><h2>Merchandising</h2></a>
+				<a href="#"><h2>Nosotros</h2></a>
+			</div>
+		</nav>
+		</>
+	)
+}
+export default Nav
