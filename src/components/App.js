d5e3dc68e029e29e705f7e744e7a67fcb8b4dd6f
CerminatiPaula <85971540+CerminatiPaula@users.noreply.github.com>
2021-10-16 13:14:19 -0300

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import Nav from "./NavBar";
+
+const App = () =>  {
+	return (
+		//se puede poner todo dentro de una etiqueta vacia. Si no esta la etiqueta vacia tira error
+		//la otra opcion es andar haciendo archivos, que es lo que se hace
+		<>
+			<Nav/>
+
+		</>
+	)
+}
+
+export default App
